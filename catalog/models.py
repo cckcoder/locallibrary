f4a5b9a94cd7b866081a2061ea6d515aef17224b
codewizz <c.chaiwat.ch@gmail.com>
2021-12-06 09:35:55 +0000

    add: class language

--- catalog/models.py
+++ catalog/models.py
@@ -25,12 +25,26 @@ class Book(models.Model):
         help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>',
     )
     genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
+    language = models.ForeignKey("Language", on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        ordering = ["title", "author"]
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[str(self.id)])
+
+
+class Language(models.Model):
+    name = models.CharField(
+        max_length=200,
+        help_text="Enter the book's natural language (e.g. English, French, Japanese etc.)",
+    )
+
+    def __str__(self):
+        return self.name
 
 
 class BookInstance(models.Model):
