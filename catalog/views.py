5571e55395c365640222426b5a911e26cca5d63a
codewizz <c.chaiwat.ch@gmail.com>
2021-12-07 22:41:04 +0000

    add: password event page; update: index login require

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from .models import *
 
 
+@login_required
 def index(request):
     num_visits = request.session.get('num_visits', 0)
     request.session['num_visits'] = num_visits + 1
